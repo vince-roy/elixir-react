dbde34fa295b339d250ee638110c9910f01aa3c9
Vince Roy <vince@potionapps.com>
2024-08-25 13:06:46 -0600

    clean up

--- config/dev.exs
+++ config/dev.exs
@@ -84,4 +84,6 @@ config :phoenix_live_view,
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
 
-import_config "dev.secret.exs"
+if File.exists?("config/dev.secret.exs") do
+  import_config "dev.secret.exs"
+end
